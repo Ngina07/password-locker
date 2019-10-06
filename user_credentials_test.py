6b44650b0502ba731506a2134cd6a47fc57015ca
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-06 22:10:05 +0300

    test whether method to save credentials to credentials list is functioning propery

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -29,21 +29,21 @@ class TestUser(unittest.TestCase):
 		Test to check if the new users info is saved into the users list
 		'''
 		self.new_user.save_user()
-		self.assertEqual(len(User.users_list),1)
+		self.assertEqual(len(User.users_list),3)
 
 class TestCredentials(unittest.TestCase):
 	'''
 	Test class that defines test cases for the credentials class behaviours.
 	Args:
-	    unittest.TestCase: helps in creating test cases
+	unittest.TestCase: helps in creating test cases
 	'''
 	def test_check_user(self):
 		'''
 		Function to test whether the login in function check_user works as expected
 		'''
-		self.new_user = User('Phillis','Ngina','Ngish1234')
+		self.new_user = User('Phillis', 'Ngina', 'Ngish1234')
 		self.new_user.save_user()
-		user2 = User('James','Njoroge','Njosh1234')
+		user2 = User('James', 'Njoroge', 'Njosh1234')
 		user2.save_user()
 
 		for user in User.users_list:
@@ -52,6 +52,30 @@ class TestCredentials(unittest.TestCase):
 		return current_user
 
 		self.assertEqual(current_user,Credential.check_user(user2.password,user2.first_name))
+
+	def setUp(self):
+		'''
+		Function to create an accounts credentials before each test
+		'''
+		self.new_credential = Credential('Harlem', 'Twitter', 'Harlemy', 'Harlem1234')
+
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of credential instances is properly done
+		'''
+		self.assertEqual(self.new_credential.user_name,'Harlem')
+		self.assertEqual(self.new_credential.site_name,'Twitter')
+		self.assertEqual(self.new_credential.account_name,'Harlemy')
+		self.assertEqual(self.new_credential.password,'Harlem1234')
+
+	def test_save_credentials(self):
+		'''
+		Test to check if the new credential information is saved into credentials list
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('Harlem','Twitter','Harlemy','Harlem1234')
+		twitter.save_credentials()
+		self.assertEqual(len(Credential.credentials_list),2)
 
 if __name__ == '__main__':
     unittest.main()
