74e84c84277b40bb3c9e771300375158d11bfc32
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-06 22:08:59 +0300

    add method to save credentials to credentials list

--- credentials.py
+++ credentials.py
@@ -1,4 +1,5 @@
 import string
+# from user import User
 
 class Credential:
 	'''
@@ -20,24 +21,24 @@ class Credential:
 				current_user = user.first_name
 		return current_user
 
-	# def __init__(self,user_name,site_name,account_name,password):
+	def __init__(self,user_name,site_name,account_name,password):
 
-	# 	'''
-	# 	Method to define the properties for each user object will hold.
-	# 	'''
+		'''
+		Method to define the properties for each user object will hold.
+		'''
 
-	# 	# instance variables
-	# 	self.user_name = user_name
-	# 	self.site_name = site_name
-	# 	self.account_name = account_name
-	# 	self.password = password
+		# instance variables
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = password
 
-	# def save_credentials(self):
-	# 	'''
-	# 	save_credentials method saves a newly created users to credentials_list
-	# 	'''
-	# 	# global users_list
-	# 	Credential.credentials_list.append(self)
+	def save_credentials(self):
+		'''
+		save_credentials method saves a newly created users to credentials_list
+		'''
+		# global users_list
+		Credential.credentials_list.append(self)
 	
 	# def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
 	# 	'''
