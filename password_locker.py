16496faefd6f9c51311739e4aa33a5fe4d256e79
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-07 00:37:22 +0300

    add function that saves and displays credentials

--- password_locker.py
+++ password_locker.py
@@ -24,6 +24,28 @@ def verify_user(firstname,lastname,password):
 	verify_user = Credential.check_user(firstname,lastname,password)
 	return verify_user
 
+def create_credential (user_name, site_name, account_name, password):
+	'''
+	Function creates new credential
+	'''
+	new_credential = Credential ( user_name, site_name, account_name, password)
+	return new_credential
+
+def save_credential(credential):
+	'''
+	Function to save a newly created credential
+	'''
+	Credential.save_credentials (credential)
+
+def display_credentials(user_name):
+	'''
+	Function to display credentials created and saved by the user
+	'''
+	return Credential.display_credentials (user_name)
+
+
+
+
 
 
 
