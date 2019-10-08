1edca3b31f22f0a91a776748ec2a63ccc27863d7
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-08 08:40:10 +0300

    Add delete method to delete credentials on user command

--- password_locker.py
+++ password_locker.py
@@ -1,91 +1,138 @@
-#!/usr/bin/env python3.6
-
 import pyperclip
 from user import User
 from credentials import Credential
+import string
 
-def create_user(firstname,lastname,password):
+def create_user(username,password):
 	'''
 	Function to create a new user account
 	'''
-	new_user = User(firstname,lastname,password)
+	new_user = User(username,password)
 	return new_user
 
 def save_user(user):
 	'''
 	Function to save new user account
 	'''
-	User.save_user(user)
+	user.save_user()
 
-def verify_user(firstname,password):
+def user_exist (name):
 	'''
-	Function that verifies the existence of the user before creating user credentials
+	Checks if user exists
 	'''
-	verify_user = Credential.check_user(firstname,password)
-	return verify_user
+	return User.user_exists(name)
+		
+# def verify_user(firstname,password):
+# 	'''
+# 	Function that verifies the existence of the user before creating user credentials
+# 	'''
+# 	verify_user = Credential.check_user(firstname,password)
+# 	return verify_user
 
-def create_credential (user_name, site_name, account_name, password):
+def create_credential (account, userName, password):
 	'''
 	Function creates new credential
 	'''
-	new_credential = Credential ( user_name, site_name, account_name, password)
+	new_credential = Credential ( account, userName, password)
 	return new_credential
+
 
 def save_credential(credential):
 	'''
 	Function to save a newly created credential
 	'''
-	Credential.save_credentials (credential)
+	Credential.save_credentials ()
 
-def display_credentials(user_name):
+def find_credentials():
 	'''
-	Function to display credentials created and saved by the user
+	Method to find credentials
 	'''
-	return Credential.display_credentials (user_name)
+	return Credential.find_credentials(account)
 
-def main():
-	print(' ')
-	print('******Hey there, welcome to password locker*****')
-	while True:
-		print(' ')
-		print('I would like to: \n 1-Create an Account \n 2-Log In \n 3-Exit-app')
-		print(' ')
-		short_code = input('Enter a choice: ').lower().strip()
-		if short_code == '3':
-			break
+def check_credentials(account):
+	'''
+	Checks if credential exists
+	'''
+	return Credential.credential_exists(account)
 
-		elif short_code == '1':
-			print("-"*69)
-			print(' ')
-			print("let's create you an account")
-			first_name = input('Enter your first name - ').strip()
-			last_name = input('Enter your last name - ').strip()
-			password = input('Enter your password - ').strip()
-			save_user(create_user(first_name,last_name,password))
-			print(" ")
-			print(f'New Account Created for: {first_name} {last_name}  password: {password}')
-			print("-"*69)
+def delete_credentials(credential):
+	'''
+	Deletes credential after user command 
+	'''
+	return Credential.delete_credential(credential)
 
-		elif short_code == '2':
-			print("-"*60)
-			print(' ')
-			print('To login, enter your account details:')
-			firstname = input('Enter your first name - ').strip()
-			password = str(input('Enter your password - '))
-			user_exists = verify_user(firstname,password)
-			if user_exists == firstname:
-				print(" ")
-				print(f'Welcome {firstname}. Please choose an option to continue.')
-				print(' ')
-			else: 
-				print ("Wrong details")
+# def display_credentials(user_name):
+# 	'''
+# 	Function to display credentials created and saved by the user
+# 	'''
+# 	return Credential.display_credentials (user_name)
 
-		else: 
-			print ("Wrong details")
+# def generate_password(self):
+# 			'''
+# 			Method to generate an 8 character password for a user 
+# 			'''
+# 			ge_password = Credential.generate_password()
+# 			return ge_password
 
+			
+# def main():
+# 			print(' ')
+# 			print('******Hey there, welcome to password locker***** I would like to: \n 1-Create an Account \n 2-Log In \n 3-Exit')
+# 			short_code = input().lower().strip()
+# 			if short_code == "1":
+# 				print("Enter username")
+# 				user_name = input()
+# 				while True:
+# 					print(" TP - To type your own password:\n GP - To generate random Password")
+# 					short_code = input().lower().strip()
+# 					if selected_choice  == 'tp':
+# 					password = input("Input your password")
+# 					break
+# 				elif selected_choice == "gp":
+# 					password = generate_password(password)
+# 					break
+# 				else: 
+# 					print("Password error")
+# 					save_user(create_user(first_name,last_name,password))
+# 					print(" ")
+# 					print(f"New Account Created for: {first_name} {last_name}  password: {password}")
+# 					print("-"*69)
 
-if __name__ == '__main__':
-	main()
+# 			elif short_code == '2':
+# 					print("-"*60)
+# 					print(' ')
+# 					print('To login, enter your account details:')
+# 					firstname = input('Enter your first name - ').strip()
+# 					password = str(input('Enter your password - '))
+# 					login = login_user(user_name,password)
+# 					if login_user == login:
+# 						print(" ")	
+# 						print(f'Welcome {firstname}.')
+# 						print(' ')
+# 			while True
+# 						print(" CR - to create new credentials: \n DC to Display credentials: \n RM- Delete credentials: \n CP- Copy credentials to clipboard")	
+# 						short_code = input().lower().strip()
+# 						if short_code == "cr":
+# 							print("create new credentials")
+# 							print("account name....")
+# 							account = input().lower().strip()
+# 							print("Your account username")
+# 							userName = input()
+# 							while True:
+# 								print(" TP - To type your own pasword if you already have an account:\n GP - To generate random Password")
+# 								password_Choice = input().lower().strip()
+# 								if password_Choice == 'tp':
+# 									password = input("Enter Your Own Password\n")
+# 									break
+# 								elif password_Choice == 'gp':
+# 									password = generate_password(password)
+# 									break
+# 					else:
+# 						print("Invalid password please try again")
+# 				save_credentials(create_new_credential(account,userName,password))
+
+# 		if __name__ == '__main__':
+# 			main()
 
 
 
