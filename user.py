88bbc3f3e75d9d910514b903a4474d2863640ac2
Ngina07 <phillis.njoroge@strathmore.edu>
2019-10-08 08:41:37 +0300

    add method to check if user exists in users list

--- user.py
+++ user.py
@@ -2,21 +2,36 @@ class User:
 	'''
 	Class that generates user accounts and save their information
 	'''
-	# Class Variables
-	# global users_list
+	
 	users_list = []
-	def __init__(self,first_name,last_name,password):
+	# def setUp(self):
+	# 	'''
+	# 	'''
+	# 	self.new_user = User("Ngina","Phillis","ngina123")
+
+	def __init__(self,username,password):
 		'''
 		Method to define the properties that each user object will hold.
 		'''
 
 		# instance variables
-		self.first_name = first_name
-		self.last_name = last_name
+		self.username = username
+		# self.account = account
 		self.password = password
 
 	def save_user(self):
-		'''
-		save_user method saves a newly created users to users_list
+		''' 
+		save_user method saves newly created users to users_list
 		'''
 		User.users_list.append(self)
+
+	def delete_user(self):
+		User.users_list.remove(self)
+	
+	@classmethod 
+	def user_exists(cls,name):
+		for user in cls.users_list:
+			if user.username == name :
+				return True
+		return False
+
